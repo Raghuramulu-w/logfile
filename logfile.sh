b98e31261a84e884941639d85ecd4b5e24c7700e
raghuramulu-w <raghu0711@gmail.com>
2024-07-16 13:41:29 +0530

    hi

--- logfile.sh
+++ logfile.sh
@@ -12,6 +12,6 @@ FILES_TO_DELETE=$(find $SOURCE_FILE -type f -mtime +14 -name "*.log")
 while IFS= read -r line
 do 
    echo "deleting file:$line "
-   rm -rf $line
+   #rm -rf $line
 
 done <<< $FILES_TO_DELETE
