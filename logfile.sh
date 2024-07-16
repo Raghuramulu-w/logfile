bcea72d05156145082bc1eed74d22d02fcfbe487
raghuramulu-w <raghu0711@gmail.com>
2024-07-16 13:20:24 +0530

    logfiles

---
+++ logfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SOURCE_FILE="/tmp/sourcefile.log"
+if [ ! -d $SOURCE_FILE]
+ then 
+ echo "folder not exit"
+fi
+FILES_TO_DELETE=$(find $SOURCE_FILE -type f -mtime +14 -name "*.log")
+while IFS read -r line
+do 
+   echo "deleting file :  $line
+   rm -r $line
+
+done<<<$FILES_TO_DELETE
